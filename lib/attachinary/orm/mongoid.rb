2c95a7d7c8523a1032bdd37c9c0c3439f227ac68
Jeff Gran <jgran@doximity.com>
2025-05-22 07:54:54 -0600

    remove mongoid files

--- lib/attachinary/orm/mongoid.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'file_mixin'
-require_relative 'base_extension'
-require_relative 'mongoid/extension'
-require_relative 'mongoid/file'
-
-Mongoid::Document::ClassMethods.send :include, Attachinary::Extension
